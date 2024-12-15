ca59afb67c6aadddc3ec32beab3d952385d02b33
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2024-12-15 18:21:34 +0530

    added the logging system

--- app/book/routes.py
+++ app/book/routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, Blueprint, render_template, redirect, url_for, jsonify, session, flash
+from flask import Flask, Blueprint, render_template, redirect, url_for, jsonify, session, flash, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from app.models import Session, Book
+from app.models import Session, Book, User
 from app.forms import BookForm
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -13,24 +13,31 @@ book_bp = Blueprint('book', __name__)
 @book_bp.route('/add', methods=['GET', 'POST'])
 @jwt_required()
 def add():
-    if 'user_id' not in session:
+    user_id = get_jwt_identity()
+    user = Session.query(User).get(user_id)
+    
+    if not user:
+        current_app.logger.warning("Unauthorized user attempted to add a book.")
         return redirect(url_for('auth.Login'))
     
     form = BookForm()
     if form.validate_on_submit():
-            try:
-                new_book = Book(title=form.title.data,
-                                description=form.description.data,
-                                user_id=session['user_id']
-                                )
-                Session.add(new_book)
-                Session.commit()
-                flash('book added successfully')
-                return redirect(url_for('book.get_books'))
-            except Exception as e:
-                flash(f'book addition failed: {str(e)}')
-                Session.rollback()
+        try:
+            new_book = Book(title=form.title.data,
+                            description=form.description.data,
+                            user_id=user_id)
+            Session.add(new_book)
+            Session.commit()
+            current_app.logger.info(f"Book added by {user.username}: {user_id} : {new_book.title}")
+            flash("Book added successfully!", "success")
+            return redirect(url_for('book.get_books'))  # Redirect to a list view or home
+        except Exception as e:
+            Session.rollback()
+            current_app.logger.error(f"Failed to add book for user {user.username}: {str(e)}")
+            flash("An error occurred while adding the book. Please try again.", "error")
+    
     return render_template('book.html', form=form)
+
 
 @book_bp.route('/display', methods=['GET'])
 @jwt_required()
@@ -52,5 +59,5 @@ def delete(id):
     delete_book = Session.query(Book).filter_by(user_id=user_id).filter_by(id=id).first()
     Session.delete(delete_book)
     Session.commit()
-    flash('Book deleted successfully')
+    current_app.logger.info(f"Book deleted by user {user_id}: {delete_book.title}")
     return redirect(url_for('book.get_books'))
